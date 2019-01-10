958dffaf950b542c47c15250c3a2fbaa1ec1beb7
1 <1>
2019-01-10 16:45:50 +0800

    cyt

---
+++ js/index.js
@@ -0,0 +1,12 @@
+
+// $(document).ready(function(){
+//     $(".row a").click(function(){
+            
+//         var href = $(this).attr("href");
+//         var top = $(href).offset().top;
+    
+    
+//         $("html,body").animate({scrollTop:top},500)
+//         // return false;
+//     })
+// })
